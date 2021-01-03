0407c3cc4996db690c63f7cc658f6715021e90ae
Vadim Dvorovenko <Vadimon@mail.ru>
2021-01-03 20:49:51 +0700

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_ipaddresslist';
-$plugin->version   = 2018011900;
-$plugin->release = '1.2';
+$plugin->version   = 2021010300;
+$plugin->release = '1.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2014051200; // Moodle 2.7.
